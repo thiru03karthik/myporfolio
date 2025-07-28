152d0fcdeddfa047e82cb1db30826cf99ac81b9b
thiru03karthik <thiru03karthik@gmail.com>
2025-07-28 15:40:56 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,7 @@
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 import Script from 'next/script'
-
 import '@/styles/tailwind.css'
-
-// export const metadata: Metadata = {
-//   title: {
-//     template: '%s - Spencer Sharp',
-//     default:
-//       'Spencer Sharp - Software designer, founder, and amateur astronaut',
-//   },
-//   description:
-//     'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
-//   alternates: {
-//     types: {
-//       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-//     },
-//   },
-// }
-
 
 export default function RootLayout({
   children,
@@ -29,7 +12,19 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <head>
         <meta name="google-site-verification" content="CW_w-xwJSaTjhjkagMfQRfI-dntye-s95AEztvwZ51s" />
-        {/* Google Analytics Script */}
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PS7XSBN8');
+          `}
+        </Script>
+
+        {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-4Z5LKHW52J"
@@ -50,6 +45,16 @@ export default function RootLayout({
         />
       </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PS7XSBN8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Providers>
           <div className="flex w-full">
             <Layout>{children}</Layout>
