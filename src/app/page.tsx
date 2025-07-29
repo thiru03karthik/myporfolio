b8c6d4518294ce8d510ea753ee074455b6944004
thiru03karthik <thiru03karthik@gmail.com>
2025-07-29 07:27:28 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -251,7 +251,7 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="KARTHIK Digital Marketer.pdf" variant="secondary" className="group mt-6 w-full">
+      <Button href="public/files/KARTHIK Digital Marketer.pdf" variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
